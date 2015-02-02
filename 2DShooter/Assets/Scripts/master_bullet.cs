a652a643f49e81669f155f39cb4b2b3bf8bec3a3
ctdh24 <chuyn006@ucr.edu>
2015-02-02 15:14:16 -0800

    Basic player stuff

---
+++ 2DShooter/Assets/Scripts/master_bullet.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class master_bullet : MonoBehaviour {
+	public float damage;
+	public float fireRate;
+	private string creator;
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+		rigidbody.velocity = transform.forward * speed;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Destroy (this.gameObject, 2);
+	}
+	public void isCreator( string name ) {
+		creator = name;
+	}
+	void OnTriggerEnter( Collider target ) {
+		if (target.name == "Enemy" && creator == "Player") {
+
+		}
+	}
+	public float FireRate() {
+		return fireRate;
+	}
+}
