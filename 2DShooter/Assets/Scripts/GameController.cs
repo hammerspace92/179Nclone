5755fea1872bf729130e827c65e30d9285876d7a
WinrichSy <wsy001@ucr.edu>
2015-02-05 01:17:37 -0800

    Asteroids, assets from asset store, boxed
    Accidentally removed Animations.meta, Base.meta, Scenes.meta,
    Particles,meta, terrain.meta, Either than that. I included an actual
    enemy, random waves, physical interaction between player and enemies,
    explosion particles, new assets from asset store

---
+++ 2DShooter/Assets/Scripts/GameController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+	public GameObject hazard;
+	public Vector3 spawnValues;
+	public int hazardCount;
+	public float spawnWait;
+	public float startWait;
+
+	void Start()
+	{
+		StartCoroutine (SpawnWaves ());
+	}
+
+	IEnumerator SpawnWaves ()
+	{
+			yield return new WaitForSeconds (startWait);
+			while (true) {
+					for (int i = 0; i<hazardCount; i++) {
+							Vector3 spawnPosition = new Vector3 (spawnValues.x, Random.Range (-6, 6), spawnValues.z);
+							Quaternion spawnRotation = Quaternion.identity;
+							Instantiate (hazard, spawnPosition, spawnRotation);
+							yield return new WaitForSeconds (spawnWait);
+					}
+
+			}
+	}
+}
